e5817cb129f5e7d194696c6616b1e787fa92a9b8
GLOBAL\C5322996 <roy.yadid@sap.com>
2022-12-04 16:12:53 +0200

    design question app

--- ClientSide/src/app/welcome/welcome.component.ts
+++ ClientSide/src/app/welcome/welcome.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component
-Angular 12 Quiz Application | Angular step-by-step Project | Quiz Application from Scratch.css']
+  styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent {
 
