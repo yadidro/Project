32a71a97dcb562a14dd3a9dd5dc4ec9127123a27
GLOBAL\C5322996 <roy.yadid@sap.com>
2022-12-04 14:19:45 +0200

    start addiing  components

---
+++ ClientSide/src/app/welcome/welcome.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component
+Angular 12 Quiz Application | Angular step-by-step Project | Quiz Application from Scratch.css']
+})
+export class WelcomeComponent {
+
+}
